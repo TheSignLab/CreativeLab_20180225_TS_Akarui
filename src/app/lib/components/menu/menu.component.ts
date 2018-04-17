94123f40363d43f1a4ade426f4ef8ae2050133ee
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-04-17 18:03:35 -0500

    Update favicon

--- src/app/lib/components/menu/menu.component.ts
+++ src/app/lib/components/menu/menu.component.ts
@@ -11,9 +11,31 @@ export class MenuComponent implements OnInit {
   public MenuInstagramImgPath = require('../../../../assets/img/menu/icon_instagram.png');
   public MenuFacebookImgPath = require('../../../../assets/img/menu/icon_facebook.png');
   public MenuTwitterImgPath = require('../../../../assets/img/menu/icon_twitter.png');
+
+  public menuState = 'closed';
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  public toogleMenu(): void {
+    if (this.menuState === 'closed') {
+      this.openMenu();
+      this.menuState = 'opened';
+    } else {
+      this.closeMenu();
+      this.menuState = 'closed';
+    }
+  }
+
+  private openMenu(): void {
+    console.log('ngReport : The <menu> is showing up ... ');
+  }
+
+  private closeMenu(): void {
+    console.log('ngReport : The <menu> is closing up ... ');
+
   }
 
 }
