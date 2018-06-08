1714ea4205e8cd391fa7131f512a3bb05bcd80ec
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-06-08 16:43:05 -0500

    updarServer

--- server.js
+++ server.js
@@ -4,14 +4,29 @@ const express = require('express');
 
 const app = express();
 
+const path = require('path');
 
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
 // Serve only the static files form the dist directory
 
 app.use(express.static(__dirname + '/dist'));
 
-
+app.use(forceSSL());
 
 // Start the app by listening on the default Heroku port
 
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
